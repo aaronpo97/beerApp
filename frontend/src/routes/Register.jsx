06a0f037c3868e2c78b786dac5acd7817bb5077d
Aaron Po <76038768+aaronpo97@users.noreply.github.com>
2022-01-14 17:55:03 -0500

    Fixing client side validation for registration
    user registration

--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,19 +1,10 @@
-import { Grid } from '@mui/material';
+import { Grid, Container } from '@mui/material';
 import { useState } from 'react';
-<<<<<<< HEAD
 import { useNavigate } from 'react-router';
+import ms from 'ms';
 
 import RegistrationForm from '../components/registration/RegistrationForm';
-import { Grid, Container } from '@mui/material';
-=======
-import SideImage from '../components/misc/SideImage';
->>>>>>> 9ec18256559f920e60b5af3b3ef5deb92c13b31b
 
-import PageOne from '../components/registration/PageOne';
-import PageTwo from '../components/registration/PageTwo';
-import PageThree from '../components/registration/PageThree';
-
-<<<<<<< HEAD
 const Register = () => {
   const blocklistedWords = ['1^Ce9T]Re-J|']; //test phrase
 
@@ -42,9 +33,13 @@ const Register = () => {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
       const checkUserInDB = async (usernameQuery, emailQuery) => {
-        const response = await fetch(`http://localhost:5000/users/doesuserexist?username=${usernameQuery}&email=${emailQuery}`);
-        const data = await response.json();
-        return data.payload;
+        try {
+          const response = await fetch(`http://localhost:5000/users/doesuserexist?username=${usernameQuery}&email=${emailQuery}`);
+          const data = await response.json();
+          return data.payload;
+        } catch (error) {
+          return { message: 'username and/or email was blank.' };
+        }
       };
 
       const checkDateOfBirth = () => {
@@ -89,34 +84,39 @@ const Register = () => {
         errors.lastName = 'last name is required.';
       }
 
-      setFormErrors(errors);
+      if (Object.keys(errors).length) {
+        setFormErrors(errors);
+        throw new Error('Form validation failed.');
+      }
     };
 
-    // const registerUser = async () => {
-    //   const url = 'http://localhost:5000/users/register';
-    //   const requestOptions = {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(registrationFormValues),
-    //   };
+    const registerUser = async () => {
+      const url = 'http://localhost:5000/users/register';
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(registrationFormValues),
+      };
 
-    //   const response = await fetch(url, requestOptions);
+      const response = await fetch(url, requestOptions);
 
-    //   if (response.status !== 201) throw new Error('Oops!');
+      if (response.status !== 201) throw new Error('Oops!');
+    };
 
-    //   const data = await response.json();
+    const handleLoginAndRedirect = data => {
+      //login user with new credentials
+      localStorage['access-token'] = data.payload.accessToken;
+      localStorage['refresh-token'] = data.payload.refreshToken;
 
-    //   //login user with new credentials
-    //   localStorage['access-token'] = data.payload.accessToken;
-    //   localStorage['refresh-token'] = data.payload.refreshToken;
+      //take user to profile page, where they are prompted to check email for confirmation link
+      navigate(`/users/${data.payload.newUser._id}`);
+    };
 
-    //   //take user to profile page, where they are prompted to check email for confirmation link
-    //   navigate(`/users/${data.payload.newUser._id}`);
-    // };
-
-    validateData().then(() => {
-      console.log('suh dude');
-    });
+    //function calls
+    validateData()
+      .then(() => registerUser())
+      .then(data => handleLoginAndRedirect(data))
+      .catch(err => console.log(err));
   };
 
   return (
@@ -136,63 +136,6 @@ const Register = () => {
       </Grid>
     </Container>
   );
-=======
-const RegisterPageSelection = () => {
-	const [username, setUsername] = useState('');
-	const [dateOfBirth, setDateOfBirth] = useState(null);
-	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('');
-	const [email, setEmail] = useState('');
-	const [pageNum, setPageNum] = useState(1);
-
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-
-	switch (pageNum) {
-		case 1:
-			return (
-				<PageOne
-					username={username}
-					setUsername={setUsername}
-					dateOfBirth={dateOfBirth}
-					setDateOfBirth={setDateOfBirth}
-					pageNum={pageNum}
-					setPageNum={setPageNum}
-					firstName={firstName}
-					setFirstName={setFirstName}
-					lastName={lastName}
-					setLastName={setLastName}
-				/>
-			);
-		case 2:
-			return (
-				<PageTwo
-					password={password}
-					setPassword={setPassword}
-					email={email}
-					setEmail={setEmail}
-					confirmPassword={confirmPassword}
-					setConfirmPassword={setConfirmPassword}
-					pageNum={pageNum}
-					setPageNum={setPageNum}
-				/>
-			);
-		case 3:
-			return <PageThree />;
-	}
-};
-const Register = () => {
-	return (
-		<Grid container component='main' sx={{ height: '100vh' }}>
-			<SideImage
-				imageUrl={
-					'https://images.pexels.com/photos/5659494/pexels-photo-5659494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-				}
-			/>
-			<RegisterPageSelection />
-		</Grid>
-	);
->>>>>>> 9ec18256559f920e60b5af3b3ef5deb92c13b31b
 };
 
 export default Register;
